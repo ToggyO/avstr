41eb33a92d413015436633683320b408f977d1da
Негодов Никита <nn@smarthead.ru>
2020-03-13 15:30:32 +0300

    Раскоментил код

---
+++ src/Core/common/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NavBar = ({ btnHandler }) => {
+    return (
+        <nav>
+
+        </nav>
+    );
+};
+
+NavBar.propTypes = {
+
+};
+
+export default NavBar;
