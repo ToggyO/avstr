8add57bc66d91a9d7325a663a78c6a0ff39a3dc7
Негодов Никита <nn@smarthead.ru>
2020-03-16 13:19:40 +0300

    Добавил обработку серверных ошибок в логауте / MRIS-140

--- src/Core/authorization/sagas/handleLogout.js
+++ src/Core/authorization/sagas/handleLogout.js
@@ -1,5 +1,6 @@
-import { call } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
 import api from '../../api';
+import { setErrMessage } from '../action-creators';
 
 const { REACT_APP_AUTH_API } = process.env;
 
@@ -11,8 +12,14 @@ function* handleLogout() {
         });
 
         window.location = '/';
-    } catch (err) {
-        // console.log(err);
+    } catch ({ type }) {
+        switch (type) {
+            case 'ServerError':
+                yield put(setErrMessage('Что то пошло не так. Пожалуйста поробуйте позже.'));
+                break;
+            default:
+                break;
+        }
     }
 }
 
