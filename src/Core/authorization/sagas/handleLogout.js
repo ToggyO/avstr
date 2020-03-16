1ef2dfe0894a1f39a64d7864e0ac887cf5f97d18
Негодов Никита <nn@smarthead.ru>
2020-03-16 12:00:39 +0300

    Конфиг линта/ MRIS-140

--- src/Core/authorization/sagas/handleLogout.js
+++ src/Core/authorization/sagas/handleLogout.js
@@ -3,6 +3,7 @@ import api from '../../api';
 
 const { REACT_APP_AUTH_API } = process.env;
 
+
 function* handleLogout() {
     try {
         const { isOk } = yield call(api.get, `${REACT_APP_AUTH_API}/account/logout`, {
