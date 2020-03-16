d4f898eb000497692938e9fe4c3a2da54e52c3e8
Негодов Никита <nn@smarthead.ru>
2020-03-16 13:09:41 +0300

    Убрал лишние комменты / MRIS-140

--- src/Core/authorization/userManager.js
+++ src/Core/authorization/userManager.js
@@ -2,24 +2,6 @@ import { createUserManager } from 'redux-oidc';
 
 const { REACT_APP_URL, REACT_APP_AUTH_URL } = process.env;
 
-/* const userManagerConfig = {
-    client_id: 'avastar-frontend',
-    redirect_uri: 'http://localhost:3000/callback',
-    response_type: 'token id_token',
-    scope: 'openid avastar-microservices',
-    authority: 'http://localhost:3759',
-    post_logout_redirect_uri: 'http://localhost:3000',
-}; */
-
-/* const userManagerConfig = {
-    client_id: 'avastar-frontend',
-    redirect_uri: 'http://avastar.smartheadtest.ru/callback',
-    response_type: 'token id_token',
-    scope: 'openid avastar-microservices',
-    authority: 'http://accounts.avastar.smartheadtest.ru',
-    post_logout_redirect_uri: 'http://avastar.smartheadtest.ru',
-}; */
-
 const userManagerConfig = {
     client_id: 'avastar-frontend',
     redirect_uri: `${REACT_APP_URL}/callback`,
