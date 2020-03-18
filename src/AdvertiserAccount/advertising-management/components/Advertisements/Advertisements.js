490d310fbbe2e203bb20303a912966cd68987ba4
Негодов Никита <nn@smarthead.ru>
2020-03-18 10:47:10 +0300

    Добавил стилей / MRIS-138

--- src/AdvertiserAccount/advertising-management/components/Advertisements/Advertisements.js
+++ src/AdvertiserAccount/advertising-management/components/Advertisements/Advertisements.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'semantic-ui-react';
+import { Button, Icon } from 'semantic-ui-react';
 
 import Container from '../../../../Core/common/Container/Container';
-import NavBar from '../../../../Core/common/NavBar/NavBar';
 import Title from '../../../../Core/common/Title/Title';
 
 import styles from './Advertisements.module.scss';
 
 
-const Advertisements = ({ title, navBarBtnHandler, addBtnHandler }) => (
+const Advertisements = ({ title, addBtnHandler }) => (
     <Container>
-        <NavBar handleLogoutClick={navBarBtnHandler} />
-        <Title text={title} />
-        <Button
-            className={styles.btn}
-            onClick={addBtnHandler}
-        >
-            Добавить
-        </Button>
+        <div className={styles.wrap}>
+            <Title
+                className={styles.title}
+                text={title}
+            />
+            <Button
+                className={styles.btn}
+                onClick={addBtnHandler}
+            >
+                <Icon name="plus" />
+                Добавить
+            </Button>
+        </div>
     </Container>
 );
 
 
 Advertisements.propTypes = {
     title: PropTypes.string.isRequired,
-    navBarBtnHandler: PropTypes.func.isRequired,
     addBtnHandler: PropTypes.func.isRequired,
 };
-
 
 export default Advertisements;
