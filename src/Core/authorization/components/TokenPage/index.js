7b6408d714937ac5b32a08c89689c15f61bf9eb7
Негодов Никита <nn@smarthead.ru>
2020-03-18 19:53:17 +0300

    Теперь текст выводится в див / MRIS-183

--- src/Core/authorization/components/TokenPage/index.js
+++ src/Core/authorization/components/TokenPage/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'semantic-ui-react';
 
 import userManager from '../../userManager';
 
 const TokenPage = () => {
+    const [text, setText] = useState('');
     const handleBtnClick = () => {
-        userManager.getUser().then((user) => alert(user.access_token));
+        userManager.getUser().then((user) => setText(user.access_token));
     };
 
     return (
         <div>
             <h1>Сервисная страничка</h1>
+            <div
+                style={{ marginBottom: '20px' }}
+            >
+                {text}
+            </div>
             <Button onClick={handleBtnClick}>Принести токен :)</Button>
         </div>
     );
