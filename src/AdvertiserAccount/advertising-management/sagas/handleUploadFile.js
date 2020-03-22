0537837110e666363c667e5fe125e61d3718aecc
Негодов Никита <nn@smarthead.ru>
2020-03-22 23:16:58 +0300

    Реализовал eventChannel для связи саги с событиями XHR / MRIS-141

--- src/AdvertiserAccount/advertising-management/sagas/handleUploadFile.js
+++ src/AdvertiserAccount/advertising-management/sagas/handleUploadFile.js
@@ -1,38 +1,77 @@
-import { put, call } from 'redux-saga/effects';
+import { eventChannel, END } from 'redux-saga';
+import { put, call, take } from 'redux-saga/effects';
 import api from 'Core/api';
-import handleRequestErrors from 'Core/api/handeRequestErrors';
+// import handleRequestErrors from 'Core/api/handeRequestErrors';
 import { changeUploadStatus } from '../action-creators';
 
 const { REACT_APP_API } = process.env;
 
 
-function* handleUploadFile({ data: { advertisementText, file } }) {
-    yield put(changeUploadStatus('Pending'));
+function createUploadChanel(xhr, file) {
+    return eventChannel((emitter) => {
+        const onProgress = (e) => {
+            if (e.lengthComputable) {
+                const progress = `${e.loaded} / ${e.total}`;
+                emitter({ progress });
+            }
+        };
 
+        const onFailure = () => {
+            emitter({ err: true });
+            emitter(END);
+        };
+
+        const onSuccess = () => {
+            const { status, response } = xhr;
+            if (status === 201) {
+                emitter({ success: { response } });
+                emitter(END);
+            } else {
+                onFailure(null);
+            }
+        };
+
+        xhr.upload.addEventListener('progress', onProgress);
+        xhr.upload.addEventListener('error', onFailure);
+        xhr.upload.addEventListener('abort', onFailure);
+        xhr.addEventListener('loadend', onSuccess);
+
+        xhr.send(file);
+
+        return () => {
+            xhr.upload.removeEventListener('progress', onProgress);
+            xhr.upload.removeEventListener('error', onFailure);
+            xhr.upload.removeEventListener('abort', onFailure);
+            xhr.removeEventListener('loadend', onSuccess);
+            xhr.abort();
+        };
+    });
+}
+
+
+function* handleUploadFile({ data: { advertisementText, file } }) {
     const formData = new FormData();
     formData.append('file', file);
     formData.append('name', advertisementText);
 
-    const req = yield call(api.configurePostFile, `${REACT_APP_API}/advertiser-microservice/advertisements`, 'json');
+    const configuredRequest = yield call(api.configurePostFile, `${REACT_APP_API}/advertiser-microservice/advertisements`, 'json');
 
-    try {
-        req.upload.onprogress = ({ loaded, total }) => {
-            console.log(`Отправлено ${loaded} из ${total}`);
-        };
+    const channel = yield call(createUploadChanel, configuredRequest, formData);
 
-        req.onloadend = () => {
-            const { status } = req;
-            if (status === 201) {
-                // changeUploadStatus('Success');
-                console.log('Отправлено успешно!');
-            } else {
-                // changeUploadStatus('Error');
-                console.log('Ошибка при отправке');
-            }
-        };
-        req.send(formData);
-    } catch (err) {
-        handleRequestErrors(req.status);
+    while (true) {
+        const { progress = 0, err, success } = yield take(channel);
+
+        if (success) {
+            const { response } = success;
+            console.log(response);
+            yield put(changeUploadStatus('Success'));
+            return;
+        }
+        if (err) {
+            yield put(changeUploadStatus('Error'));
+            return;
+        }
+        yield put(changeUploadStatus(progress));
     }
 }
 
