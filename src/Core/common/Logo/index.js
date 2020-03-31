3a3f6e4846711933275520243b2cc5dae05a1d7e
Негодов Никита <nn@smarthead.ru>
2020-03-31 17:05:01 +0300

    Добавил логотип / MRIS-211

--- src/Core/common/Logo/index.js
+++ src/Core/common/Logo/index.js
@@ -2,24 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 
+import { ReactComponent as IconLogo } from './logo.svg';
+
 import styles from './index.module.scss';
 
 
-const Logo = ({ className }) => (
-    <div className={cn(styles.logo, className)}>
-        <img
-            src=""
-            alt="AVAStar logo"
+const Logo = ({ size, className, ...attrs }) => {
+    const logoClasses = cn(
+        styles.logo,
+        styles.normal,
+        { [styles.small]: size === 'small' },
+        className,
+    );
+
+    return (
+        <IconLogo
+            size
+            className={logoClasses}
+            {...attrs}
         />
-    </div>
-);
+    );
+};
 
 
 Logo.defaultProps = {
+    size: 'normal',
     className: '',
 };
 
 Logo.propTypes = {
+    size: PropTypes.string,
     className: PropTypes.string,
 };
 
