6935cbb448135a0326472334af384ed975318bd5
Негодов Никита <nn@smarthead.ru>
2020-04-07 19:27:51 +0300

    Добавил разметку для пагинации / MRIS-143

--- src/DevicesAccount/devices-managment/components/DevicesPagination/index.js
+++ src/DevicesAccount/devices-managment/components/DevicesPagination/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Icon from 'Core/common/Icon';
 
-const DevicesPagination = ({ pagination }) => {
-    console.log(pagination);
-    return (
-        <div />
-    );
-};
+import styles from './index.module.scss';
+
+const DevicesPagination = ({
+    pagination: {
+        page,
+        total,
+        size,
+        hasPrevious,
+        hasNext,
+    },
+}) => (
+    <div className={styles.wrap}>
+        <div>
+            {`Показано устройств: ${size} из 10000`}
+        </div>
+        <div className={styles.controls}>
+            {hasPrevious && <Icon name="arrowLeft" />}
+            <div className={styles.text}>
+                {`Страница ${page} из ${total}`}
+            </div>
+            {hasNext && (
+                <>
+                    <Icon name="arrowRight" />
+                    <Icon name="arrowRightDouble" />
+                </>
+            )}
+
+        </div>
+    </div>
+);
+
 
 DevicesPagination.propTypes = {
-    pagination: PropTypes.shape().isRequired,
+    pagination: PropTypes.shape({
+        page: PropTypes.number.isRequired,
+        total: PropTypes.number.isRequired,
+        size: PropTypes.number.isRequired,
+        hasPrevious: PropTypes.bool.isRequired,
+        hasNext: PropTypes.bool.isRequired,
+    }).isRequired,
 };
 
 export default DevicesPagination;
