8f097b3d42387a42195859eafc7d6517b141ea02
Негодов Никита <nn@smarthead.ru>
2020-04-07 11:17:21 +0300

    Добавил верстку страницы "Новое устройство" / MRIS-144

--- src/DevicesAccount/devices-managment/components/NewDevice/index.js
+++ src/DevicesAccount/devices-managment/components/NewDevice/index.js
@@ -1,56 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 
 import Container from 'Core/common/Container';
 import Title from 'Core/common/Title';
 import Input from 'Core/common/Input';
 import Button from 'Core/common/Button';
+import { Icon } from 'semantic-ui-react';
+import NewDeviceTextItem from '../NewDeviceTextItem';
 
 import styles from './index.module.scss';
 
-const NewDevice = () => (
-    <Container>
-        <Title
-            text="Новое устройство"
-            className={styles.title}
-        />
-        <div>
-            <span>1.</span>
-            Включите устройство
-        </div>
-        <Input />
-        <div>
-            <span>2.</span>
-            Введите код с экрана устройства
-        </div>
-        <div>
-            <span>3.</span>
-            Придумайте название
-        </div>
-        <Input />
 
-        <div>
-            <Button
-                type="outline"
-                size="medium"
-                // className={styles.declineBtn}
-                // onClick={}
-            >
-                Отменить
-            </Button>
+const NewDevice = () => {
+    const [codeText, setCodeText] = useState('');
+    const [deviceNameText, setDeviceNameText] = useState('');
 
-            <Button
-                type="main"
-                size="medium"
-                // disabled={}
-                // className={styles.saveBtn}
-                // onClick={}
-            >
-                Далее
-            </Button>
-        </div>
-    </Container>
-);
+    const handleCodeChange = ({ target: { value } }) => {
+        setCodeText(value);
+    };
+
+    const handleDeviceNameChange = ({ target: { value } }) => {
+        setDeviceNameText(value);
+    };
+
+    return (
+        <Container className={styles.newDevice}>
+            <Title
+                text="Новое устройство"
+                className={styles.title}
+            />
+            <NewDeviceTextItem
+                number={1}
+                text="Включите устройство"
+                className={styles.firstPoint}
+            />
+
+            <NewDeviceTextItem
+                number={2}
+                text="Введите код с экрана устройства"
+                className={styles.otherPoints}
+            />
+            <Input
+                className={styles.firstInput}
+                value={codeText}
+                onChange={handleCodeChange}
+            />
+
+            <NewDeviceTextItem
+                number={3}
+                text="Придумайте название"
+                className={styles.otherPoints}
+            />
+            <Input
+                placeholder="Устройство 1"
+                className={styles.secondInput}
+                value={deviceNameText}
+                onChange={handleDeviceNameChange}
+            />
+
+            <div>
+                <Button
+                    type="outline"
+                    size="medium"
+                    className={styles.declineBtn}
+                    // onClick={}
+                >
+                    Отменить
+                </Button>
+
+                <Button
+                    type="main"
+                    size="medium"
+                    disabled={!codeText || !deviceNameText}
+                    className={styles.okBtn}
+                    // onClick={}
+                >
+                    Далее
+                    <Icon name="arrow circle right" />
+                </Button>
+            </div>
+        </Container>
+    );
+};
 
 
 NewDevice.propTypes = {};
---
+++ src/DevicesAccount/devices-managment/components/NewDeviceTextItem/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
+import styles from './index.module.scss';
+
+const NewDeviceTextItem = ({ number, text, className }) => (
+    <div className={cn(styles.wrap, className)}>
+        <span className={styles.number}>
+            {number}
+            {'. '}
+        </span>
+        {text}
+    </div>
+);
+
+
+NewDeviceTextItem.defaultProps = {
+    className: '',
+};
+
+NewDeviceTextItem.propTypes = {
+    number: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    className: PropTypes.string,
+};
+
+export default NewDeviceTextItem;
