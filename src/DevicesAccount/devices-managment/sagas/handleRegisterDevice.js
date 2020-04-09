8ccf21c23fd4b71f2ea7cd64025b2068e1f38fc1
Негодов Никита <nn@smarthead.ru>
2020-04-09 16:16:24 +0300

    Разделил саги по получению статуса на разные файлы / MRIS-148

--- src/DevicesAccount/devices-managment/sagas/handleRegisterDevice.js
+++ src/DevicesAccount/devices-managment/sagas/handleRegisterDevice.js
@@ -1,68 +1,11 @@
-import {
-    call,
-    put,
-    delay,
-    fork,
-    take,
-    cancel,
-    cancelled,
-} from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
 
 import api from 'Core/api';
 import { changeDeviceStatus } from '../action-creators';
-import { CANCEL_DEVICE_REGISTRATION } from '../actions';
+import handleDeviceStatusRequest from './handleDeviceStatusRequest';
+
 
 const { REACT_APP_DEVICE_API } = process.env;
-
-function* requestDeviceStatus(deviceId) {
-    try {
-        let isRequestsFinish = false;
-        let isRegistered = false;
-
-        setTimeout(() => {
-            isRequestsFinish = true;
-        }, 10000);
-
-        while (!isRequestsFinish) {
-            const statusRes = yield call(api.get, `${REACT_APP_DEVICE_API}/device-management-microservice/Devices/IsConnected/${deviceId}`);
-            const { content: { isConnected } } = statusRes;
-            if (isConnected) {
-                isRequestsFinish = true;
-                isRegistered = isConnected;
-            }
-            yield delay(1000);
-        }
-
-        if (isRegistered) {
-            yield put(changeDeviceStatus('connected'));
-        } else {
-            yield put(changeDeviceStatus('notConnected'));
-        }
-    } catch ({ type }) {
-        switch (type) {
-            case 'AuthorizationError':
-                window.location = '/';
-                break;
-            case 'ServerError':
-                alert('На сервере произошла ошибка.');
-                break;
-            default:
-                break;
-        }
-    } finally {
-        if (yield cancelled()) {
-            yield put(changeDeviceStatus(''));
-        }
-    }
-}
-
-
-function* handleDeviceStatusRequest(deviceId) {
-    const requestStatusChain = yield fork(requestDeviceStatus, deviceId);
-    yield take(CANCEL_DEVICE_REGISTRATION);
-    yield cancel(requestStatusChain);
-}
-
 
 function* handleRegisterDevice({ data }) {
     try {
