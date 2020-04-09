b5cb282d8255dae081342c0ff7b7386b98179aca
Негодов Никита <nn@smarthead.ru>
2020-04-09 12:51:01 +0300

    Реализовал отправку запроса на получение статуса устр-ва (1 запрос в сек, 10сек) / MRIS-148

--- src/DevicesAccount/devices-managment/sagas/handleRegisterDevice.js
+++ src/DevicesAccount/devices-managment/sagas/handleRegisterDevice.js
@@ -1,4 +1,4 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, delay } from 'redux-saga/effects';
 
 import api from 'Core/api';
 import { receiveDeviceStatus, receiveDeviceSerial } from '../action-creators';
@@ -8,18 +8,33 @@ const { REACT_APP_DEVICE_API } = process.env;
 
 function* handleRegisterDevice({ data }) {
     try {
-        const response = yield call(api.post, `${REACT_APP_DEVICE_API}/device-management-microservice/devices`, data);
+        const registerRes = yield call(api.post, `${REACT_APP_DEVICE_API}/device-management-microservice/devices`, data);
         yield put(receiveDeviceStatus('pending'));
 
-        const { content: { id } } = response;
+        const { content: { id } } = registerRes;
         if (!id || id <= 0) {
             yield put(receiveDeviceStatus('notConnected'));
             return;
         }
 
-        const response2 = yield call(api.get, `${REACT_APP_DEVICE_API}/device-management-microservice/Devices/IsConnected/${id}`);
-        const { content: { isConnected } } = response2;
-        if (isConnected) {
+
+        let isRequestsFinish = false;
+        let isRegistered = false;
+        setTimeout(() => {
+            isRequestsFinish = true;
+        }, 10000);
+
+        while (!isRequestsFinish) {
+            const statusRes = yield call(api.get, `${REACT_APP_DEVICE_API}/device-management-microservice/Devices/IsConnected/${id}`);
+            const { content: { isConnected } } = statusRes;
+            if (isConnected) {
+                isRequestsFinish = true;
+                isRegistered = isConnected;
+            }
+            yield delay(1000);
+        }
+
+        if (isRegistered) {
             const { serialNumber } = data;
             yield put(receiveDeviceSerial(serialNumber));
             yield put(receiveDeviceStatus('connected'));
