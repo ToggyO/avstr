8ebe72880ffdba22965d7e5dd664564a3e9da17a
Негодов Никита <nn@smarthead.ru>
2020-04-10 16:23:34 +0300

    Разделил саги-вотчеры для фич кабинета девайсов / MRIS-144

--- src/DevicesAccount/devices-managment/sagas/devicesManagementWatcher.js
+++ src/DevicesAccount/devices-managment/sagas/devicesManagementWatcher.js
@@ -4,7 +4,7 @@ import { REQUEST_DEVICES, REGISTER_DEVICE } from '../actions';
 import handleRequestDevices from './handleRequestDevices';
 import handleRegisterDevice from './handleRegisterDevice';
 
-export default function* advertisingManagementWatcher() {
+export default function* devicesManagementWatcher() {
     yield takeLatest(REQUEST_DEVICES, handleRequestDevices);
     yield takeLatest(REGISTER_DEVICE, handleRegisterDevice);
 }
