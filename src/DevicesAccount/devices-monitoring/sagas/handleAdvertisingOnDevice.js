1f6708af70669a145fd95ad4799057e40fd9d3d6
Негодов Никита <nn@smarthead.ru>
2020-04-17 11:02:56 +0300

    Правка саг  / MRIS-274

--- src/DevicesAccount/devices-monitoring/sagas/handleAdvertisingOnDevice.js
+++ src/DevicesAccount/devices-monitoring/sagas/handleAdvertisingOnDevice.js
@@ -1,15 +1,18 @@
-import { call } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
 
 import api from 'Core/api';
 
 import handleRequestDeviceContent from './handleRequestDeviceContent';
+import { changeAdvertisingLoaderStatus } from '../action-creators';
 
 const { REACT_APP_DEVICE_API } = process.env;
 
 
 function* handleAdvertisingOnDevice({ data }) {
     try {
+        yield put(changeAdvertisingLoaderStatus(true));
         yield call(api.post, `${REACT_APP_DEVICE_API}/device-management-microservice/devices/ChangeAdvertisementsState`, { id: data });
+        yield put(changeAdvertisingLoaderStatus(false));
         yield* handleRequestDeviceContent({ data });
     } catch ({ type }) {
         switch (type) {
--- src/DevicesAccount/devices-monitoring/sagas/handleRequestDeviceContent.js
+++ src/DevicesAccount/devices-monitoring/sagas/handleRequestDeviceContent.js
@@ -1,16 +1,14 @@
 import { call, put } from 'redux-saga/effects';
 
 import api from 'Core/api';
-import { receiveDeviceContent, changeAdvertisingLoaderStatus } from '../action-creators';
+import { receiveDeviceContent } from '../action-creators';
 
 const { REACT_APP_DEVICE_API } = process.env;
 
 
 function* handleRequestDeviceContent({ data }) {
     try {
-        yield put(changeAdvertisingLoaderStatus(true));
         const { content } = yield call(api.get, `${REACT_APP_DEVICE_API}/device-management-microservice/devices/${data}`);
-        yield put(changeAdvertisingLoaderStatus(false));
         yield put(receiveDeviceContent(content));
     } catch ({ type }) {
         switch (type) {
