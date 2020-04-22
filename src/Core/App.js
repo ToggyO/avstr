40441e651187528e5508141eb76a22abe0b3f9e1
Негодов Никита <nn@smarthead.ru>
2020-04-22 17:30:59 +0300

    Добавил глобальную переменную io / MRIS-276

--- src/Core/App.js
+++ src/Core/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Provider } from 'react-redux';
-import './App.scss';
+
+import io from 'socket.io-client';
 import 'semantic-ui-css/semantic.min.css';
 import 'Core/scss/fonts/add-fonts.scss';
-
 import store from './store';
 import RootRouter from './root/RootRouter';
+
+import './App.scss';
 // import configureMirage from './api/configureMirage';
 // configureMirage();
+
+window.io = io;
 
 const App = () => (
     <Provider store={store}>
