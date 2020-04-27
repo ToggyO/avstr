0b3cbf14d69d2ef92146dd5498ebf278d9251698
Негодов Никита <nn@smarthead.ru>
2020-04-27 14:16:25 +0300

    Упростил компонент видео (сделал его функциональным) / MRIS-276

--- src/DevicesAccount/devices-monitoring/components/DeviceMonitoringVideo.js
+++ src/DevicesAccount/devices-monitoring/components/DeviceMonitoringVideo.js
@@ -1,53 +1,39 @@
-import React, { Component, createRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import streamStore from 'Core/streamStoreService';
 
 
-class DeviceMonitoringVideo extends Component {
-    constructor(props) {
-        super(props);
-        this.ref = createRef(null);
-    }
+const DeviceMonitoringVideo = ({ mediaStreamId, cancelMediaStream }) => {
+    const ref = useRef(null);
 
-    componentDidMount() {
-        const { mediaStreamId } = this.props;
+    useEffect(() => {
         const { getStream } = streamStore;
-        const { current } = this.ref;
+        const { current } = ref;
         current.srcObject = getStream(mediaStreamId);
-    }
 
-    componentDidUpdate() {
-        const { mediaStreamId } = this.props;
-        const { getStream } = streamStore;
-        const { current } = this.ref;
-        current.srcObject = getStream(mediaStreamId);
-    }
+        return () => {
+            cancelMediaStream();
 
-    componentWillUnmount() {
-        const { cancelMediaStream, mediaStreamId } = this.props;
-        cancelMediaStream();
+            const connection = streamStore.getConnection(mediaStreamId);
+            if (connection) {
+                connection.closeSocket();
+                connection.onstream = null;
+                connection.onstreamended = null;
+                connection.onMediaError = null;
+                connection.error = null;
+            }
+        };
+    }, [mediaStreamId, cancelMediaStream]);
 
-        const connection = streamStore.getConnection(mediaStreamId);
-        if (connection) {
-            connection.closeSocket();
-            connection.onstream = null;
-            connection.onstreamended = null;
-            connection.onMediaError = null;
-            connection.error = null;
-        }
-    }
-
-    render() {
-        return (
-            // eslint-disable-next-line jsx-a11y/media-has-caption
-            <video
-                ref={this.ref}
-                autoPlay
-                controls
-            />
-        );
-    }
-}
+    return (
+        // eslint-disable-next-line jsx-a11y/media-has-caption
+        <video
+            ref={ref}
+            autoPlay
+            controls
+        />
+    );
+};
 
 
 DeviceMonitoringVideo.defaultProps = {
