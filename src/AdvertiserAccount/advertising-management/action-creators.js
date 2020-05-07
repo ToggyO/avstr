6ce65e3b9d816964bec88f12bc1297fe315d4c64
Негодов Никита <nn@smarthead.ru>
2020-05-07 16:24:59 +0300

    Реализовал отмену загрузки файла при уходе со страницы добавления объявления / MRIS-141

--- src/AdvertiserAccount/advertising-management/action-creators.js
+++ src/AdvertiserAccount/advertising-management/action-creators.js
@@ -2,6 +2,8 @@ import {
     UPLOAD_FILE,
     CHANGE_UPLOAD_STATUS,
     RECEIVE_UPLOADED_CONTENT,
+    SAVE_XHR,
+    CLEAN_XHR,
     REQUEST_ADVERTISEMENTS,
     RECEIVE_ADVERTISEMENTS,
     DELETE_ADVERTISEMENT,
@@ -12,14 +14,20 @@ export const uploadFile = (data) => ({
     type: UPLOAD_FILE,
     data,
 });
-
 export const changeUploadStatus = (data) => ({
     type: CHANGE_UPLOAD_STATUS,
     data,
 });
-
 export const receiveUploadedContent = (data) => ({
     type: RECEIVE_UPLOADED_CONTENT,
+    data,
+});
+export const saveXhr = (data) => ({
+    type: SAVE_XHR,
+    data,
+});
+export const cleanXhr = (data) => ({
+    type: CLEAN_XHR,
     data,
 });
 
