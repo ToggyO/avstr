befa33b4c28bf0ebd4c33e26eaf51345fdd21307
Негодов Никита <nn@smarthead.ru>
2020-05-20 19:11:56 +0300

    Заменил таблицу на антовскую / MRIS-420

--- src/DevicesAccount/devices-managment/components/DevicesList/index.js
+++ src/DevicesAccount/devices-managment/components/DevicesList/index.js
@@ -1,42 +1,89 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import DeviceItem from '../DeviceItem';
+import history from 'Core/history';
 
-import styles from './index.module.scss';
+// import Icon from 'Core/common/Icon';
+
+import { Table } from 'antd';
+import { RightOutlined } from '@ant-design/icons';
+// import DeviceItem from '../DeviceItem';
+
+// import styles from './index.module.scss';
 import DevicesPagination from '../DevicesPagination';
 
 
-const DevicesList = ({ pagination, devices, requestDevices }) => (
-    <>
-        <DevicesPagination
-            pagination={pagination}
-            requestDevices={requestDevices}
-        />
+const DevicesList = ({ pagination, devices, requestDevices }) => {
+    const handleArrowClick = (id) => {
+        history.push(`/devices/monitoring/${id}`);
+    };
 
-        <table className={styles.table}>
-            <thead>
-                <tr className={styles.head}>
-                    <th className={styles.name}>Название</th>
-                    <th className={styles.serial}>Серийный номер</th>
-                    <th className={styles.status}>Статус</th>
-                    <th>{}</th>
-                </tr>
-            </thead>
-            <tbody className={styles.body}>
-                {devices.map((device) => {
-                    const { id } = device;
-                    return (
-                        <DeviceItem
-                            content={device}
-                            key={id}
-                        />
-                    );
-                })}
-            </tbody>
-        </table>
-    </>
-);
+    const columns = [
+        {
+            title: 'Название',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Серийный номер',
+            dataIndex: 'serialNumber',
+            key: 'serialNumber',
+        },
+        {
+            title: 'Статус',
+            dataIndex: 'status',
+            key: 'status',
+        },
+        {
+            title: '',
+            dataIndex: 'arrow',
+            key: 'arrow',
+            render: (id) => {
+                console.log(id);
+                return (
+                    <RightOutlined onClick={handleArrowClick} />
+                );
+            },
+        },
+    ];
+
+    return (
+        <>
+            <DevicesPagination
+                pagination={pagination}
+                requestDevices={requestDevices}
+            />
+
+            <Table
+                dataSource={devices}
+                columns={columns}
+                pagination={false}
+            />
+
+            {/* <table className={styles.table}>
+                <thead>
+                    <tr className={styles.head}>
+                        <th className={styles.name}>Название</th>
+                        <th className={styles.serial}>Серийный номер</th>
+                        <th className={styles.status}>Статус</th>
+                        <th>{}</th>
+                    </tr>
+                </thead>
+                <tbody className={styles.body}>
+                    {devices.map((device) => {
+                        const { id } = device;
+                        return (
+                            <DeviceItem
+                                content={device}
+                                key={id}
+                            />
+                        );
+                    })}
+                </tbody>
+            </table> */}
+        </>
+    );
+};
 
 
 DevicesList.propTypes = {
