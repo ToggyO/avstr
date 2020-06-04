3c058200b909dd84ea555dd37c804d7c2f37fc12
Oleg <togushakov@magora-systems.com>
2020-06-04 18:10:16 +0700

    [MRIS-583] Добавлен компонент-обертка для элементов форм

--- src/Core/ant/components/index.js
+++ src/Core/ant/components/index.js
@@ -1,3 +1,4 @@
 export * from './Layouts';
 export { AntDropdown } from './Dropdown';
-export { StandardForm } from './FormComponents/ContextForm';
+export { StandardForm, StandardFormContext } from './FormComponents/ContextForm';
+export { FormItemWrapper } from './FormComponents/FormItemWrapper';
