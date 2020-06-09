0cbb6906961871225f4ca07ce4289b34266a0cb5
Негодов Никита <nn@smarthead.ru>
2020-06-09 15:27:40 +0300

    Добавил обработчики для перехода по стр в хэдере / MRIS-467

--- src/Core/ant/components/Layouts/LoginLayout/index.jsx
+++ src/Core/ant/components/Layouts/LoginLayout/index.jsx
@@ -1,4 +1,4 @@
-// TODO(toleg): заменить s на style в импорте стилей
+// TODO(nn): поправить обработчики, когда переработаем роутинг
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Layout, Button } from 'antd';
@@ -10,33 +10,55 @@ import styles from './index.module.scss';
 
 const { Header, Content } = Layout;
 
-const LoginLayout = ({ children }) => (
-    <>
-        <Helmet>
-            <title>AVAStar - Login</title>
-            <meta name="description" content="AVAStar - Login" />
-        </Helmet>
+const LoginLayout = ({ children }) => {
+    const registerBtnHandler = () => {
+        // временно пока не переработан роутинг
+        window.location = '/advertiser-registration';
+    };
 
-        <Layout>
-            <Header className={styles.header}>
-                <div className={styles.header__container}>
-                    <a href="/" className={styles.header__logo}>
-                        <AvaLogo />
-                    </a>
-                    <div className={styles.header__buttons}>
-                        <Button type="default" ghost>Регистрация</Button>
-                        <Button type="primary">Вход</Button>
+    const entryBtnHandler = () => {
+        // временно пока не переработан роутинг
+        window.location = '/';
+    };
+    return (
+        <>
+            <Helmet>
+                <title>AVAStar - Login</title>
+                <meta name="description" content="AVAStar - Login" />
+            </Helmet>
+
+            <Layout>
+                <Header className={styles.header}>
+                    <div className={styles.header__container}>
+                        <a href="/" className={styles.header__logo}>
+                            <AvaLogo />
+                        </a>
+                        <div className={styles.header__buttons}>
+                            <Button
+                                type="default"
+                                ghost
+                                onClick={registerBtnHandler}
+                            >
+                                Регистрация
+                            </Button>
+                            <Button
+                                type="primary"
+                                onClick={entryBtnHandler}
+                            >
+                                Вход
+                            </Button>
+                        </div>
                     </div>
-                </div>
-            </Header>
-            <Content className={styles.content}>
-                <div className={styles.children_container}>
-                    {children}
-                </div>
-            </Content>
-        </Layout>
-    </>
-);
+                </Header>
+                <Content className={styles.content}>
+                    <div className={styles.children_container}>
+                        {children}
+                    </div>
+                </Content>
+            </Layout>
+        </>
+    );
+};
 
 LoginLayout.propTypes = {
     children: PropTypes.element,
