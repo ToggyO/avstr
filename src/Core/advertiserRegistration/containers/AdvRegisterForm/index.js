767b19ebcdc8774492b3d57ea33607377dbca384
Негодов Никита <nn@smarthead.ru>
2020-06-10 17:37:38 +0300

    Добавил валидацию полей / MRIS-467

--- src/Core/advertiserRegistration/containers/AdvRegisterForm/index.js
+++ src/Core/advertiserRegistration/containers/AdvRegisterForm/index.js
@@ -1,37 +1,64 @@
 // todo(nn): Добавить ссылки на документы, когда они появятся
-import React from 'react';
-// import { connect } from 'react-redux';
-import { Button } from 'antd';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import { StandardForm, FormItemWrapper } from 'Core/ant';
+// import { connect } from 'react-redux';
 // import { getProp } from 'Core/utils/getProp';
 
+import { Form, Button } from 'antd';
+import { StandardForm, FormItemWrapper } from 'Core/ant';
 import options from './options';
 
 import styles from './index.module.scss';
 
 const AdvRegisterForm = () => {
+    const [form] = Form.useForm();
+    const [, forceUpdate] = useState(); // To disable submit button at the beginning.
+
+    useEffect(() => {
+        forceUpdate({});
+    }, []);
+
+
+    const onFinishFailed = () => {
+
+    };
+
     const onSubmit = () => {
         // sendLink(values);
     };
+
+    const isBtnDisabled = () => {
+        console.log(!form.isFieldsTouched());
+        console.log(form.getFieldsError().filter(({ errors }) => errors.length).length);
+        return (form.isFieldsTouched()
+            || form.getFieldsError().filter(({ errors }) => errors.length).length);
+    };
+
 
     return (
         <div className={styles.container}>
             <div className={styles.headlines}>
                 <h1>Регистрация рекламодателя</h1>
             </div>
-            <StandardForm onFinish={onSubmit} options={options}>
+            <StandardForm
+                outerFormInstance={form}
+                options={options}
+                onFinish={onSubmit}
+                onFinishFailed={onFinishFailed}
+            >
                 <FormItemWrapper type="text-input" name="name" />
                 <FormItemWrapper type="text-input" name="surname" />
                 <FormItemWrapper type="text-input" name="organisation" />
                 <FormItemWrapper type="text-input" name="email" />
                 <FormItemWrapper type="password-input" name="password" />
                 <FormItemWrapper
+                    shouldUpdate
                     type="custom-component"
                     name="submit"
                     component={(props) => (
                         <Button
+                            disabled={isBtnDisabled()}
                             // loading={loading}
                             className={styles.submit}
                             {...props}
--- src/Core/advertiserRegistration/containers/AdvRegisterForm/options.js
+++ src/Core/advertiserRegistration/containers/AdvRegisterForm/options.js
@@ -1,33 +1,128 @@
+const warning = 'Обязательное поле';
+const isNameValidated = (value) => /^[a-zа-яё]+$/i.test(value);
+
 const formOptions = {
     name: {
         props: {
             size: 'large',
             placeholder: 'Имя',
         },
+        rules: [
+            {
+                required: true,
+                // при устройстве кастомной валидации нужно оставлять пробел в поле message
+                message: ' ',
+            },
+            {
+                validator: (_, value) => {
+                    if (!value) return Promise.reject(warning);
+
+                    if (!isNameValidated(value)) {
+                        return Promise.reject('Имя не соответствует требованиям');
+                    }
+
+                    return Promise.resolve();
+                },
+            },
+        ],
     },
     surname: {
         props: {
             size: 'large',
             placeholder: 'Фамилия',
         },
+        rules: [
+            {
+                required: true,
+                message: ' ',
+            },
+            {
+                validator: (_, value) => {
+                    if (!value) return Promise.reject(warning);
+
+                    if (!isNameValidated(value)) {
+                        return Promise.reject('Фамилия не соответствует требованиям');
+                    }
+
+                    return Promise.resolve();
+                },
+            },
+        ],
     },
     organisation: {
         props: {
             size: 'large',
             placeholder: 'Организация',
         },
+        rules: [
+            {
+                required: true,
+                message: ' ',
+            },
+            {
+                validator: (_, value) => {
+                    if (!value) return Promise.reject(warning);
+
+                    // const condition = /\w+/g.test(value);
+                    /* if (condition) {
+                        return Promise.reject('Имя организации не соответствует требованиям');
+                    } */
+
+                    return Promise.resolve();
+                },
+            },
+        ],
     },
     email: {
         props: {
             size: 'large',
             placeholder: 'Электронная почта',
         },
+        rules: [
+            {
+                required: true,
+                message: ' ',
+            },
+            {
+                validator: (_, value) => {
+                    if (!value) return Promise.reject(warning);
+
+                    const condition = /^[!#$%&'*+-/=?^_{|}~.\w]+@\w+\.\w+$/.test(value);
+                    if (!condition) {
+                        return Promise.reject('Почта не соответствует требованиям');
+                    }
+
+                    return Promise.resolve();
+                },
+            },
+        ],
     },
     password: {
         props: {
             size: 'large',
             placeholder: 'Придумайте пароль',
         },
+        rules: [
+            {
+                required: true,
+                message: ' ',
+            },
+            {
+                validator: (_, value) => {
+                    if (!value) return Promise.reject(warning);
+
+                    const condition = (
+                        /(?=.*[0-9])(?=.*[!"#$%&'()*+,-.:;<=>?@^_`{|}~])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/g
+                            .test(value)
+                    );
+                    if (!condition) {
+                        return Promise.reject('Пароль не соответствует требованиям');
+                    }
+
+                    return Promise.resolve();
+                },
+            },
+        ],
     },
     submit: {
         size: 'large',
