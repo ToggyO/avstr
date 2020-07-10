776115bac6e8b3a7ec02c8aa030e1272a96cf79e
Негодов Никита <nn@smarthead.ru>
2020-07-10 17:50:48 +0300

    Добавил кнопку для повторной регистрации в сообщение об отправке письма / MRIS-789

--- src/Core/advertiserRegistration/RegisterPage/components/InfoMessage/index.js
+++ src/Core/advertiserRegistration/RegisterPage/components/InfoMessage/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Result } from 'antd';
+import PropTypes from 'prop-types';
+import { Button, Result } from 'antd';
+
+import history from 'Core/history';
+import { ROOT_ROUTES } from 'Core/constants';
 
 import styles from './index.module.scss';
 
-const InfoMessage = () => {
+const InfoMessage = ({ setDefaultStateAction }) => {
     let date = new Date();
     date.setDate(date.getDate() + 1);
     date = date.toLocaleString('ru', {
@@ -13,6 +17,11 @@ const InfoMessage = () => {
         hour: 'numeric',
         minute: 'numeric',
     });
+
+    const registerBtnHandler = () => {
+        setDefaultStateAction();
+        history.push(ROOT_ROUTES.AD_REGISTRATION);
+    };
 
     return (
         <div className={styles.container}>
@@ -30,8 +39,23 @@ const InfoMessage = () => {
                 до&nbsp;
                 {date}
             </p>
+            <Button
+                className={styles.btn}
+                type="default"
+                onClick={registerBtnHandler}
+            >
+                Зарегестрироваться еще раз
+            </Button>
         </div>
     );
+};
+
+InfoMessage.defaultProps = {
+    setDefaultStateAction: Function.prototype,
+};
+
+InfoMessage.propTypes = {
+    setDefaultStateAction: PropTypes.func,
 };
 
 export default InfoMessage;
